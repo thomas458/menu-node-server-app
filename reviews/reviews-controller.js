513f2d454c43137dfd3808204409d74997fe1116
jiaomeizhou <zhoujiaomei@outlook.com>
2023-06-23 01:49:11 -0700

    Add recent review component to home page

--- reviews/reviews-controller.js
+++ reviews/reviews-controller.js
@@ -1,4 +1,5 @@
 import * as dao from "./reviews-dao.js"
+
 const ReviewsController = (app) => {
     const createReview = async (req, res) => {
         const review = req.body
@@ -7,19 +8,27 @@ const ReviewsController = (app) => {
         const actualReview = await dao.createReview(review)
         res.json(actualReview)
     }
-    const findReviewsByMeal = async(req, res) => {
+    const findReviewsByMeal = async (req, res) => {
         const idMeal = req.params.idMeal
         const reviews = await dao.findReviewsByMeal(idMeal)
         res.json(reviews)
     }
-    const findReviewsByAuthor = async (req, res) =>{
+    const findReviewsByAuthor = async (req, res) => {
         const author = req.params.author
         const reviews = await dao.findReviewsByAuthor(author)
         res.json(reviews)
     }
+    const findReviewsByAuthorId = async (req, res) => {
+        const currentUser = req.session['currentUser'];
+        const authorId = currentUser._id;
+        req.params.aid = authorId;
+        const reviews = await dao.findReviewsByAuthor(authorId);
+        res.json(reviews);
+    }
     app.post('/api/reviews', createReview)
     app.get('/api/meals/:idMeal/reviews', findReviewsByMeal)
     app.get('/api/users/:author/reviews', findReviewsByAuthor)
+    app.get('/api/reviews/:aid', findReviewsByAuthorId)
 
 }
 
