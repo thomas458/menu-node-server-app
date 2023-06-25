789f06064351121db3e87a138ec76b74335790c0
userwebdev <webdevnan@outlook.com>
2023-06-25 13:24:22 -0700

    request page

--- meals/meals-schema.js
+++ meals/meals-schema.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const mealsSchema = mongoose.Schema({
     name: {type:String, required: true},
     liked: {type: Boolean, default: false},
-    likes: {type: Number, default: 0}
+    likes: {type: Number, default: 0},
+    usr:{type:String}
 
 }, {collection: "meals"})
 
